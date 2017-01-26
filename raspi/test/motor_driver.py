5fc79c34fb6b10bb18eb6e71a5a32a09199dd83d
Makinori <maau3p@gmail.com>
2017-01-26 23:12:20 +0900

    [add] raspi->motor_driver

---
+++ raspi/test/motor_driver.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+
+import smbus
+import time
+
+
+## Motor Driver
+
+CW = 0   # clockwise
+CCW = 1  # counter clock wise
+
+
+bus = smbus.SMBus(1)
+addr = 0x0a
+
+dir1 = CW
+pwm1 = 0
+dir2 = CCW
+pwm2 = 128
+
+frame = 0
+
+try:
+  while True:
+
+    # set motor val
+    if pwm1 >= 255-10:
+      dir1 = CCW if dir1==CW else CW
+    pwm1 = 0 if pwm1 >= 255-10 else pwm1 + 10
+    
+    if pwm2 >= 255-10:
+      dir2 = CCW if dir2==CW else CW
+    pwm2 = 0 if pwm2 >= 255-10 else pwm2 + 10
+    
+    # i2c
+    bus.write_byte_data(addr, 0x01, dir1)
+    bus.write_byte_data(addr, 0x02, pwm1)
+    bus.write_byte_data(addr, 0x03, dir2)
+    bus.write_byte_data(addr, 0x04, pwm2)
+
+    v1 = bus.read_byte_data(addr, 0x05)
+
+    # print
+    print("pwm1 : " + str(pwm1) + " dir1 : "+str(dir1)
+          +"\t | pwm2 : " + str(pwm2) + " dir1 : "+str(dir2)
+          +" " + str(v1))
+    
+    time.sleep(0.1)
+    frame += 1
+
+
+except KeyboardInterrupt:
+pass
